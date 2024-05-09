e6b47cc61c24ee11e94d3a8f977b8340a4b98617
Sharief2854 <kadapasharief786@gmail.com>
2024-05-09 17:43:45 +0530

    second commit

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,46 +7,21 @@ function NavBar()
     serviceObj.getDetails().then((data)=>
     {
         // console.log(data.data)
-        setName(data.data[0].name)
+        setName(data.data[0].name);
     })
     return(
         <>
-            <nav class="navbar  navbar-expand-lg fixed-top">
-                <div class="container">
-                    <span>
-                        Developer 
-                        <span className='name'> {name}</span>
-                    </span>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars text-white"></i>
-                    </button>
-                    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                                Developer 
-                                <span class Name='name'> {name}</span>
-                            </h5>
-                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li class="nav-item">
-                                    <a class="link" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="link" href="#">About Me</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="link" href="#">Skills</a>
-                                </li>
-                            </ul>  
-                            <ul class="navbar-nav justify-content-end flex-grow-1 ">
-                                <li class="nav-item ">
-                                    <a className='link' id='red-color' href="#">Contact Me</a>
-                                </li>
-                            </ul>
-                        </div>
-                
+            <nav class="nav">
+                <div className='center mt-3'>
+                    <img src='images/pic1.jpg' className='nav-pic'></img>
+                    <div className='mt-2 name'>
+                        <span>{name}</span>
+                    </div>
+                    <div className='social-media mt-3'>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                        <i class="fa-solid fa-f"></i>
                     </div>
                 </div>
             </nav>
